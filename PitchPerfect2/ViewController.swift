0539136e75de649e1fecff95ddace5c486ade282
Karol Ksionek <karol.ksionek@vimn.com>
2021-06-28 22:37:52 +0200

    Initial Commit

---
+++ PitchPerfect2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PitchPerfect2
+//
+//  Created by Ksionek, Karol on 28/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
